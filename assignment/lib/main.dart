0b3b20400dbf3d750f1e6bd7e60be7428fcc26c8
anushasharma384 <sharmaanusha384@gmail.com>
2023-07-17 23:07:32 +0530

    UI Done

--- assignment/lib/main.dart
+++ assignment/lib/main.dart
@@ -11,115 +11,404 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Homeaze',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Color.fromRGBO(7, 147, 197, 1)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Homeaze'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  MyHomePage({super.key, required String title});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  int _value = 1;
+  bool mon = false;
+  bool tues = false;
+  bool wed = false;
+  bool thu = false;
+  bool fri = false;
+  bool sat = false;
+  bool sun = false;
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: SingleChildScrollView(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            Container(
+              height: height * 0.1,
+              width: double.infinity,
+              color: Colors.white,
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              "Outlet Type and Timings",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 26),
             ),
+            Container(
+              height: height * 0.01,
+              width: double.infinity,
+              color: Colors.white,
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20, top: 20, bottom: 10),
+              child: Row(
+                children: [
+                  Text(
+                    "Outlet Type",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                    textAlign: TextAlign.left,
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(
+                top: 10,
+                left: 23,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      Radio(
+                        value: 1,
+                        groupValue: _value,
+                        onChanged: (value) {
+                          setState(() {
+                            _value = value!;
+                          });
+                        },
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        "Delivery Boy Available",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14,
+                            color: Colors.black),
+                      )
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Radio(
+                        value: 2,
+                        groupValue: _value,
+                        onChanged: (value) {
+                          setState(() {
+                            _value = value!;
+                          });
+                        },
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Text(
+                        "Delivery Boy Not Available",
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14,
+                            color: Colors.black),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 25),
+              child: Row(
+                children: [
+                  Image.asset(
+                    "assets/Images/abc.png",
+                    height: 15,
+                    width: 16,
+                  ),
+                  Text(
+                    "Sorry! Currently we are not catering to these outlets.",
+                    style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.black,
+                        fontStyle: FontStyle.italic),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20, top: 20),
+              child: Row(
+                children: [
+                  Text(
+                    "Outlet Operational Hours",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                    textAlign: TextAlign.left,
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20, top: 15, bottom: 10),
+              child: Row(
+                children: [
+                  Text(
+                    "Enter Outlet open and close hours",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                    textAlign: TextAlign.left,
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20, top: 20),
+              child: Row(
+                children: [
+                  Text(
+                    "Mark Open Days",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                    textAlign: TextAlign.left,
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 20, top: 15, bottom: 10),
+              child: Row(
+                children: [
+                  Text(
+                    "Do not forget to uncheck your OFF Days!",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                    textAlign: TextAlign.left,
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10, bottom: 15, left: 35, right: 35),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    children: [
+                      Checkbox(
+                    value: mon,
+                    onChanged: (value) {
+                      setState(() {
+                        mon = value!;
+                      });
+                    },
+                    activeColor: Color.fromRGBO(7, 147, 197, 1),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                      "Monday",
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black
+                    ),
+                  ),
+                 ],
+                  ),
+                  Row(
+                    children: [
+                      Checkbox(
+                    value: tues,
+                    onChanged: (value) {
+                      setState(() {
+                        tues = value!;
+                      });
+                    },
+                    activeColor: Color.fromRGBO(7, 147, 197, 1),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                      "Tuesday",
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black
+                    ),
+                  ),
+                 ],
+                  ),
+                  Row(
+                    children: [
+                      Checkbox(
+                    value: wed,
+                    onChanged: (value) {
+                      setState(() {
+                        wed = value!;
+                      });
+                    },
+                    activeColor: Color.fromRGBO(7, 147, 197, 1),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                      "Wednesday",
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black
+                    ),
+                  ),
+                 ],
+                  ),
+                  Row(
+                    children: [
+                      Checkbox(
+                    value: thu,
+                    onChanged: (value) {
+                      setState(() {
+                        thu = value!;
+                      });
+                    },
+                    activeColor: Color.fromRGBO(7, 147, 197, 1),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                      "Thursday",
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black
+                    ),
+                  ),
+                 ],
+                  ),
+                  Row(
+                    children: [
+                      Checkbox(
+                    value: fri,
+                    onChanged: (value) {
+                      setState(() {
+                        fri = value!;
+                      });
+                    },
+                    activeColor: Color.fromRGBO(7, 147, 197, 1),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                      "Friday",
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black
+                    ),
+                  ),
+                 ],
+                  ),
+                  Row(
+                    children: [
+                      Checkbox(
+                    value: sat,
+                    onChanged: (value) {
+                      setState(() {
+                        sat = value!;
+                      });
+                    },
+                    activeColor: Color.fromRGBO(7, 147, 197, 1),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                      "Saturday",
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black
+                    ),
+                  ),
+                 ],
+                  ),
+                  Row(
+                    children: [
+                      Checkbox(
+                    value: sun,
+                    onChanged: (value) {
+                      setState(() {
+                        sun = value!;
+                      });
+                    },
+                    activeColor: Color.fromRGBO(7, 147, 197, 1),
+                  ),
+                  SizedBox(width: 10),
+                  Text(
+                      "Sunday",
+                      style: TextStyle(
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black
+                    ),
+                  ),
+                 ],
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 50,
+              width: width * 0.85,
+              child: ElevatedButton(
+                onPressed: () async {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Color.fromARGB(255, 18, 167, 222),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10))),
+                child: const Text(
+                  "Next",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: height*0.1,
+              width: double.infinity,
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
